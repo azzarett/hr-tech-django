280abadecb6e60d79a40c5b985f929ce6c680f84
Азат <zachet04@gmail.com>
2025-10-13 22:52:05 +0500

    feat: register admin models for Project, ProjectStatus, ProjectUser, ProjectTask, User, and UserRole

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, UserRole
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'has_permission_create_user', 'has_permission_create_project')
+    search_fields = ('email', 'first_name', 'last_name')
+
+@admin.register(UserRole)
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display = ('role', 'user')

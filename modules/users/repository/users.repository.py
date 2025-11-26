2bc66bf39b49b7e6a8a342adbd460e47cc7b178a
Азат <zachet04@gmail.com>
2025-11-26 19:41:00 +0500

    feat: implement user repository with methods for fetching and saving users

---
+++ modules/users/repository/users.repository.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from modules.users.domain.models import User
+
+
+class UserRepository:
+
+    @staticmethod
+    def get_by_id(user_id) -> Optional[User]:
+        return User.objects.filter(id=user_id, deleted_at__isnull=True).first()
+
+    @staticmethod
+    def get_by_email(email) -> Optional[User]:
+        return User.objects.filter(email=email, deleted_at__isnull=True).first()
+
+    @staticmethod
+    def save(user: User) -> User:
+        user.save()
+        return user
